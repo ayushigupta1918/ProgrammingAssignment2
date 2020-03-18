f48f420aaebc70d0d00dc72fe11621f67653e6a4
ayushigupta1918 <61269112+ayushigupta1918@users.noreply.github.com>
2020-03-18 22:19:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,23 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  r <- NULL
+  initialMatrix <- NULL
+  set <- function(y){
+    initialMatrix <<- x
+    x <<- y
+    r <<- NULL
+  }
+  
+  get <- function() x
+  setReverse <- function(reverse) {
+    r <<- reverse
+  }
+  getInitialMatrix <- function() initialMatrix 
+  setInitialMatrix <- function(im) initialMatrix <<- im
+  
+  getReverse <- function() r
+  list(set = set, get = get, setReverse = setReverse, getReverse = getReverse, getInitialMatrix = getInitialMatrix, setInitialMatrix = setInitialMatrix)
 
 }
 
@@ -12,4 +29,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  r <- x$getReverse()
+  message("printing r")
+  print(r)
+  previous <- x$getInitialMatrix()
+  message("printing previous")
+  print(previous)
+  current <- x$get()
+  message("printing current")
+  print(current)
+  if(!is.null(r) && is.matrix(previous) && is.matrix(current) && (dim(previous) == dim(current)) && all(previous == current)) {
+    #set previous to current
+    x$setInitialMatrix(current)
+    message("getting cached data")
+    return(r)
+  }
+  x$setInitialMatrix(current)
+  r <- solve(current)
+  x$setReverse(r)
+  r
+
 }
