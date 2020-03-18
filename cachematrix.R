667b720099fceebf5a3624dcbcea5e354d96cb7b
ayushigupta1918 <61269112+ayushigupta1918@users.noreply.github.com>
2020-03-18 23:25:13 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,9 +5,9 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   r <- NULL
-  initialMatrix <- NULL
+  initialMatrix <- NULL  #to store the previous matrix
   set <- function(y){
-    initialMatrix <<- x
+    initialMatrix <<- x  #update the previous matrix to x before updating x
     x <<- y
     r <<- NULL
   }
@@ -30,22 +30,21 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   r <- x$getReverse()
-  message("printing r")
-  print(r)
-  previous <- x$getInitialMatrix()
-  message("printing previous")
-  print(previous)
-  current <- x$get()
-  message("printing current")
-  print(current)
+  previous <- x$getInitialMatrix() #getting previous matrix 
+  current <- x$get() #getting current matrix
+  
+  #if r is set and previous matrix and current matrix are same then use cached data
   if(!is.null(r) && is.matrix(previous) && is.matrix(current) && (dim(previous) == dim(current)) && all(previous == current)) {
     #set previous to current
-    x$setInitialMatrix(current)
     message("getting cached data")
     return(r)
   }
+  
+  #if different, then update previous to current
   x$setInitialMatrix(current)
   r <- solve(current)
+  
+  #update reverse
   x$setReverse(r)
   r
 
